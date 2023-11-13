8f668b4b900477b7022b99c2967e689c5849c047
Hashir Nadeem <hashoonadeem123@gmail.com>
2023-11-13 17:09:40 +0500

    Rewriting and Compression

--- Asp.Net Core 6/Program.cs
+++ Asp.Net Core 6/Program.cs
@@ -1,53 +1,82 @@
 using Asp.Net_Core_6.CustomMiddleware;
+using Microsoft.AspNetCore.ResponseCompression;
+using System;
 using System.IO.Pipelines;
 using System.Linq;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<CustomMiddleware2>();
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+
+});
+
 var app = builder.Build();
 
-//app.UseExceptionHandler();
-app.UseStaticFiles();
-app.UseRouting();
-//app.UseAuthentication();
-//app.UseAuthorization();
+app.UseResponseCompression();
 
+/*These are the middlewares that will execute for all the requestes and response and if we use routing before that so all of these will not execute
+ and app.userouting() will map that request directly to some endpoint */
 
-app.Use(async (HttpContext context,RequestDelegate next) =>
-{
-    await context.Response.WriteAsync("Middleware1\n");
-    await next(context);
-});
+//app.UseMiddleware<CustomMiddleware2>();
+//app.UseCustomMiddleware1();
 
-app.UseMiddleware<CustomMiddleware2>();
-
-app.UseCustomMiddleware1();
-
+//adding custom response headers
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Middleware2");
+    context.Response.Headers.Add("CustomHeader", "CustomData");
     await next(context);
 });
+
+//For complex loggin we can move to custom middlewares for logging
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation(context.Request.Method);
+    logger.LogInformation(context.Request.Path);
+    await next(context);
+});
+
 
 //conditional middleware with accessing the query parameters
 app.UseWhen
     (
-     context => context.Request.Query.ContainsKey("FirstName"),
+     context => context.Request.Path.Value.Contains("/download"),
      app =>
      {
-         app.Use(async (HttpContext context,RequestDelegate next) =>
+         //URL Rewriting
+
+         app.Use(async (HttpContext context, RequestDelegate next) =>
          {
-             var temp = context.Request.Query;
-             await context.Response.WriteAsync(context.Request.Query["FirstName"]);
+             if (context.Request.Path.Value.Contains("/download"))
+             {
+                 context.Request.Path = "/downloadStream";
+             }
+             await next(context);
+         });
+
+         app.Use(async (HttpContext context, RequestDelegate next) =>
+         {
+             var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+             logger.LogInformation(context.Request.Method);
+             logger.LogInformation(context.Request.Path);
+             await next(context);
          });
      }
     ); ;
+
 //Short Circuiting Middleware
-app.Run(async (HttpContext context) =>
+/*app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("App.Run()");
-});
+    await context.Response.WriteAsync("Short Ciruiting Middleware");
+});*/
+
+
+app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
@@ -69,17 +98,28 @@ app.UseEndpoints(endpoints =>
     //write file data in response body 
     endpoints.MapGet("/downloadstream", async (HttpContext context) =>
     {
-        var filePath = "D:\\wjipoccm.3c3";
+        var filePath = "D:\\0rd1iux4.rik";
         await using var fileStream = File.OpenRead(filePath);
         await fileStream.CopyToAsync(context.Response.Body);
     });
     //write file data in response body but did not buffer data by using body writer
     endpoints.MapGet("/downloadstreamwriter", async (HttpContext context) =>
     {
-        var filePath = "D:\\wjipoccm.3c3";
+        var filePath = "D:\\0rd1iux4.rik";
         await using var fileStream = File.OpenRead(filePath);
         await fileStream.CopyToAsync(context.Response.BodyWriter);
     });
+
+    //url redirection
+    endpoints.Map("/searchengine", (HttpContext context) =>
+    {
+        context.Response.Redirect("https://www.google.com/");
+    });
+
+    endpoints.MapGet("/compresseddata", () =>
+    
+        "Hello compressed world"
+    );
 });
 
 app.Run();
