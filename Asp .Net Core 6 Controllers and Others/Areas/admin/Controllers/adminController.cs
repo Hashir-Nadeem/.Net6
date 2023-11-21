b6c08000b3072e2d9b00fe859b397ce7ab76e6a9
Hashir Nadeem <hashoonadeem123@gmail.com>
2023-11-21 14:06:22 +0500

    areas

---
+++ Asp .Net Core 6 Controllers and Others/Areas/admin/Controllers/adminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_.Net_Core_6_Controllers_and_Others.Areas.admin.Controllers
+{
+    [Area("adminpanel")]
+    [Route("{area:exists}/[controller]/[action]")]
+    public class adminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Admin Panel");
+        }
+    }
+}
